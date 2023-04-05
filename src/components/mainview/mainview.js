3e8360ffd6c52479588934c83dea92af76d294bf
Fotis Gogos <83247314+FotisGogos@users.noreply.github.com>
2023-04-05 23:13:27 +0200

    test if responsive works

--- src/components/mainview/mainview.js
+++ src/components/mainview/mainview.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+{
+  /*import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./mainview.css";
 import emailicon from "./emailicon.png";
@@ -128,4 +129,5 @@ function mainview() {
   );
 }
 
-export default mainview;
+export default mainview*/
+}
